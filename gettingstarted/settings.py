b872fb3839f9a291326daf286f667f7ccbd99644
caelin <caelouwho@gmail.com>
2020-09-10 18:32:52 -0400

    add static files

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -117,6 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 django_heroku.settings(locals())
