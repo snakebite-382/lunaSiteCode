f1b639645f8d51dee1136f888a04d7930e4ba761
caelin <caelouwho@gmail.com>
2020-09-10 20:11:49 -0400

    remove try block

--- home/views.py
+++ home/views.py
@@ -10,16 +10,16 @@ class HomePageView(View):
 	template_name = "index.html"
 	def get(self, request, *args, **kwargs):
 		# try:
-			querysetNew = Article.objects.filter(new="True", active="True")
-			querysetFeaturedArticles = Article.objects.filter(featured="True", active="True")
-			querysetFeaturedReviews =  Review.objects.filter(featured="True", active="True")
-			articleOfTheWeek = Article.objects.get(articleOfTheWeek="True", active="True")
-			context = {
-				"object_list_new": querysetNew,
-				"object_list_featured_articles": querysetFeaturedArticles,
-				"object_list_featured_reviews": querysetFeaturedReviews,
-				"article_of_the_week": articleOfTheWeek,
-			}
+		querysetNew = Article.objects.filter(new="True", active="True")
+		querysetFeaturedArticles = Article.objects.filter(featured="True", active="True")
+		querysetFeaturedReviews =  Review.objects.filter(featured="True", active="True")
+		articleOfTheWeek = Article.objects.get(articleOfTheWeek="True", active="True")
+		context = {
+			"object_list_new": querysetNew,
+			"object_list_featured_articles": querysetFeaturedArticles,
+			"object_list_featured_reviews": querysetFeaturedReviews,
+			"article_of_the_week": articleOfTheWeek,
+		}
 		# except Exception as e:
 		# 	print(e)
 		# 	context = {}
