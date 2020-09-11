91aaf5167f191f0321fc4e571db652860121949e
caelin <caelouwho@gmail.com>
2020-09-11 19:27:51 -0400

    minor fix

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -6,7 +6,6 @@ from pages.views import contact_view, about_view
 from home.views import HomePageView
 
 from pages import views as page_views
-from django.conf.urls import handler404, handler500
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
@@ -19,6 +18,3 @@ urlpatterns = [
     path('about/', about_view, name='about'),
 
 ]
-
-handler404 = page_views.error_404
-handler500 = page_views.error_404
