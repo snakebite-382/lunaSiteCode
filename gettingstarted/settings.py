da2d4b1947ac07a42eed2191eab7150c3d0dc40c
caelin <caelouwho@gmail.com>
2020-09-11 19:16:23 -0400

    production

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "SomeSecretKey"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["lunasite.herokuapp.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
