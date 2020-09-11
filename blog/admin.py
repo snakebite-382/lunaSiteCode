b0b0e2c9364893cb8bc5def21b4b9fb93995096c
caelin <caelouwho@gmail.com>
2020-09-11 11:01:28 -0400

    uncomment django_heroku

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,7 @@ from .models import Article
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'main',)
+    prepopulated_fields = {'slug': ('title',)}
 
 
 # Register your models here.
--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -126,4 +126,4 @@ STATICFILES_DIRS = (
 # from boto.s3.connection import S3Connection
 # s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
