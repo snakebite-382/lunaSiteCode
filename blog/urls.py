9b0491c4835c4208bd702bb185e80a460f3e5693
caelin <caelouwho@gmail.com>
2020-09-11 12:03:56 -0400

    switch from id to pk

--- blog/urls.py
+++ blog/urls.py
@@ -10,8 +10,8 @@ from .views import(
 
 urlpatterns = [
 	path('', BlogPostsListView.as_view(), name="blog-list"),
-	path('<int:id>/', BlogPostDetailView.as_view(), name='blog-detail'),
+	path('<int:pk>/', BlogPostDetailView.as_view(), name='blog-detail'),
 	path('create/', BlogPostCreateView.as_view(), name='blog-create'),
-	path('<int:id>/delete', BlogPostDeleteView.as_view(), name='blog-delete'),
-	path('<int:id>/update', BlogPostUpdateView.as_view(), name='blog-update'),
+	path('<int:pk>/delete', BlogPostDeleteView.as_view(), name='blog-delete'),
+	path('<int:pk>/update', BlogPostUpdateView.as_view(), name='blog-update'),
 ]
